5463b23feaac52170a357ae597456cbff049dc7e
gokuld0 <gokuld.official@gmail.com>
2024-11-07 17:47:24 +0530

    modified application file

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "This Script is used to stop already running docker container, remove them and remove the image as well"
+echo "This Script is used to stop already running docker containers, remove them, and remove the images as well"
 
+# Stop all running containers
 sudo docker stop $(sudo docker ps -q)
+
+# Remove all containers
 sudo docker rm $(sudo docker ps -a -q)
+
+# Remove all images
 sudo docker rmi $(sudo docker images -q)
